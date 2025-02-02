fbd857e5522a0ac7d554a4c9cb92c461a88bacf5
merakliLB <merakliweb1@gmail.com>
2025-02-02 06:13:27 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        id: 1,
+        name: "قمر رمضان",
+        description: "زينة رمضانية جميلة",
+        price: 80,
+        image: "https://imgur.com/trxMBnJ.png"
+    },
+    {
+        id: 2,
+        name: "عطر منزل",
+        description: "عطر مميز لتعطير المنزل",
+        price: 80,
+        image: "https://imgur.com/IKSoHfC.png"
+    },
+    {
+        id: 3,
+        name: "سلة",
+        description: "سلة أنيقة لحمل المشتريات",
+        price: 80,
+        image: "https://imgur.com/ODv06MH.png"
+    },
+    {
+        id: 4,
+        name: "سلة كبيرة",
+        description: "سلة كبيرة لحمل الأشياء الثقيلة",
+        price: 80,
+        image: "https://imgur.com/G7I904h.png"
+    },
+    {
+        id: 5,
+        name: "بيوت",
+        description: "مجسمات خشبية لديكور المنزل",
+        price: 80,
+        image: "https://imgur.com/0i9cYNI.png"
+    }
+];
